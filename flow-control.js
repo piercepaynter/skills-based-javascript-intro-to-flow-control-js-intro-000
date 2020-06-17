a1c72dcc7bd3d7e04a29aac92e2e73d096449d53
boisterous-puppet-2200 <paynterpierce@gmail.com>
2020-06-17 02:53:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,12 @@
 function basicTeenager(age) {
   if (age > 13 && age < 19){
-    context.it("You are a teenager!")
+    return "You are a teenager!"
     }
 }
 }
 
 function teenager(age) {
-  if (age === 13:19){
-    console.log("You are a teenager!")
-    }
-  else {
-  console.log("You are not a teenager")
-  }
+
 }
 
 function ageChecker(age) {
